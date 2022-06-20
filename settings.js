fb58f6f7f2ae91102f5aa37bc550a377b84ec493
farangpal <98533096+farangpal@users.noreply.github.com>
2022-06-20 07:05:06 +0000

    Update settings.js

--- settings.js
+++ settings.js
@@ -31,7 +31,7 @@ const mintInfo = {
   },
   nftsInfo = {
     active: true,
-    minValue: 0.5,
+    minValue: 0.3,
     checkMaxDay: 1,
     receiveAddress: '0x6a59B38Dd35fa489F1c840E3f2072c5Ad9CB9767',
   }
